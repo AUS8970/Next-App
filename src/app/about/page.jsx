cf5186a1f1551cb2b8962e6ee60693e64692d294
aus8970 <www.aus8970@gmail.com>
2025-02-16 22:59:45 +0600

    Accordion added from shsdcn.ui

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
-export default function page() {
+export default function AboutPage() {
   return (
-    <div>
-      
+    <div className='max-w-96 mx-auto'>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger> Accordion 1 </AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger> Accordion 2 </AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   )
 }
